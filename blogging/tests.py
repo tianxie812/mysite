88c467f898e49f1c31b4b4d898f23debf5f18c9e
tianxie812 <xietian628@yahoo.com>
2020-09-17 15:02:01 -0700

    adding blogging

--- blogging/tests.py
+++ blogging/tests.py
@@ -1,15 +1,25 @@
-from blogging.models import Post
+from blogging.models import Post, Category
 from django.test import TestCase
 from django.contrib.auth.models import User
 
+
 class PostTestCase(TestCase):
-	fixtures = ['blogging_test_fixture.json', ]
+    fixtures = ['blogging_test_fixture.json', ]
 
-	def setUp(self):
-		self.user = User.objects.get(pk=1)
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
 
-	def test_string_representation(self):
-		expected = "This is a title"
-		p1 = Post(title=expected)
-		actual = str(p1)
-		self.assertEqual(expected, actual)
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+
+
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
